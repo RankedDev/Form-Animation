d0c516bef73ed485fa529d629490701740b83078
activenoob <yusufkehinde11@gmail.com>
2020-08-04 17:07:23 +0100

    form anination

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log('hello world');
+
+function animatedForm() {
+    const arrows = document.querySelectorAll('.fa-arrow-down');
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            //check for validation
+
+            if (input.type === 'text' && validateUser(input)) {
+                // console.log('everything is okay');
+                nextSlide(parent, nextForm)
+            } else if (input.type === 'email' && validateEmail(input)) {
+                nextSlide(parent, nextForm)
+
+            } else if (input.type === 'password' && validateUser(input)) {
+                nextSlide(parent, nextForm)
+            } else {
+                parent.style.animation = 'shake 0.5s ease'
+            }
+
+            // get rid on animation
+            parent.addEventListener('animationend', () => {
+                parent.style.animation = '';
+            })
+
+        })
+    })
+
+}
+
+function validateUser(user) {
+    if (user.value.length < 6) {
+        console.log('not enough characthers');
+        error('rgb(189,87,87)');
+    } else {
+        error('rgb(87, 189, 130)')
+        return true
+    }
+}
+
+function validateEmail(email) {
+    const validation = /\S+@\S+\.\S+/;
+    if (validation.test(email.value)) {
+        error('rgb(87, 189, 130)')
+        return true
+    } else {
+        error('rgb(189,87,87)');
+
+    }
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function nextSlide(parent, nextForm) {
+    parent.classList.add('inactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active')
+}
+
+
+animatedForm();
